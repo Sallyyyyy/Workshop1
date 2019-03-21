74ad6de27c1bc412388503673c53275bb785acce
Lee szu-hsuan <sally20309@gmail.com>
2019-03-21 16:25:12 +0800

    insert bookData create #book_grid

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -16,7 +16,37 @@ function loadBookData() {
         localStorage.setItem('bookData', JSON.stringify(bookDataFromLocalStorage));
     }
 }
-
 $(function () {
     loadBookData();
+});
+$(document).ready(function () {
+    $("#book_grid").kendoGrid({
+        dataSource: bookData,
+        height: 550,
+        groupable: true,
+        sortable: true,
+        pageable: {
+            refresh: true,
+            pageSizes: true,
+            buttonCount: 5
+        },
+        columns: [{
+            template: "<div class='customer-photo'" +
+                "style='background-image: url(../content/web/Customers/#:data.CustomerID#.jpg);'></div>" +
+                "<div class='customer-name'>#: ContactName #</div>",
+            field: "ContactName",
+            title: "Contact Name",
+            width: 240
+        }, {
+                field: "BookId",
+                title: "書籍編號"
+        }, {
+                field: "BookName",
+                title: "書籍名稱"
+        }, {
+                field: "BookCategory",
+                title: "書籍種類",
+            width: 150
+        }]
+    });
 });
