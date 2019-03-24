4674b9cd16bfa1c1ed40760776efb449b0caedf6
Lee szu-hsuan <sally20309@gmail.com>
2019-03-24 15:28:03 +0800

    SET    BookPrice/BookAmount/BookTotal attributes.style ADD   toolbar: ["create"] editable: "popup" SET     BookCategory to chinese

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -1,3 +1,15 @@
+console.log(bookData);
+console.log(bookData[2].BookCategory);
+var bookDataCopy = bookData.slice();
+var i;
+for (i = 0; i < bookDataCopy.length; i++) {
+    if (bookData[i].BookCategory == "database") {
+        bookData[i].BookCategory = "資料庫";
+        console.log(i);
+        console.log(bookData[i].BookCategory);
+    }
+}
+
 var bookDataFromLocalStorage = [];
 var bookCategoryList = [
     { text: "資料庫", value: "database", src: "image/database.jpg" },
@@ -19,11 +31,50 @@ $(function () {
     loadBookData();
 });
 $(document).ready(function () {
+    var dataSource = new kendo.data.DataSource({
+        transport: {
+            read: function (e) {
+                // on success
+                e.success(bookData);
+
+                // on failure
+                //e.error("XHR response", "status code", "error message");
+            },
+            //the other CRUD settings are omitted for brevity 
+           
+            destroy: function (e) {
+                // remove items from the original datasource by using e.data
+                bookData.splice(getIndexById(e.data.BookId), 1); //splice用來刪除从 index 處開始的零個或多個元素
+                // on success
+                e.success();
+                // on failure
+                //e.error("XHR response", "status code", "error message");
+            },
+            update: function (e) {
+                // locate item in original datasource and update it
+                bookData[getIndexById(e.data.BookId)] = e.data;
+                // on success
+                e.success();
+                // on failure
+                //e.error("XHR response", "status code", "error message");
+            },
+            error: function (e) {
+                // handle data operation error
+                alert("Status: " + e.status + "; Error message: " + e.errorThrown);
+            },
+            schema: {
+                model: { id: "BookId" },
+                fields: {
+                    BookId: { editable: false, nullable: true },
+                }
+            }
+        }
+    });
     $("#book_grid").kendoGrid({
-        dataSource: new kendo.data.DataSource({
-            data: bookData
-        }),
+        dataSource: dataSource,
         height: 550,
+        toolbar: ["create"],
+        pageable: true,
         groupable: true,
         sortable: true,
         pageable: {
@@ -32,41 +83,54 @@ $(document).ready(function () {
             buttonCount: 5
         },
         columns: [
-            { command: "destroy" },
+            { command: "destroy", title: "&nbsp;" },
                 {
                     field: "BookId",
-                    title: "書籍編號"
+                    title: "書籍編號",
             }, {
                     field: "BookName",
-                    title: "書籍名稱"
+                    title: "書籍名稱",
+                    width: "250px"
             }, {
                     field: "BookCategory",
                     title: "書籍種類",
             }, {
                     field: "BookAuthor",
-                    title: "作者"
+                    title: "作者",
+                    width: "100px"
             },{
                     field: "BookBoughtDate",
-                    title: "購買日期"
+                    title: "購買日期",
+                    format: "{0: yyyy-MM-dd}"
             }, {
                     field: "BookDeliveredDate",
-                    title: "送達狀態",
+                    title: "送達狀態"
             }, {
                     field: "BookPrice",
-                    title: "金額"
+                    title: "金額",
+                    attributes: {
+                        "class": "table-cell",
+                        style: "text-align: right"
+                    },
+                    format: '{0:n0}'
             }, {
                     field: "BookAmount",
-                    title: "數量"
+                    title: "數量",
+                    attributes: {
+                        "class": "table-cell",
+                        style: "text-align: right"
+                    },
+                    format: '{0:n0}'
             }, {
                     field: "BookTotal",
-                    title: "總計"
+                    title: "總計",
+                    attributes: {
+                        "class": "table-cell",
+                        style: "text-align: right"
+                    },
+                    format: '{0:n0} 元'
                 }],
-        remove: function (e) {
-                console.log("Removing", e.model.name);
-        },
-        schema: {
-              model: { id: "id" }
-            },
-        editable: true,
+        
+        editable: "popup"
     });
 });
